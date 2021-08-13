f61862028dc9d02c6c45310e7cbd963215522af0
Leo Hays <starryhornet@gmail.com>
2021-08-13 13:52:23 -0500

    Create a CodeGrade submission

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ const App = () => {
   // the state properties here.
   const [characters, setCharacters] = useState([]);
   const [error, setError] = useState(null);
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+
   
 
 
