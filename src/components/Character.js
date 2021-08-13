a3e7553011aa141d803d5a470cca778a1cc13312
Leo Hays <starryhornet@gmail.com>
2021-08-13 13:28:56 -0500

    star wars sprint spagetti

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,47 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const StyledCharacters = styled.div`
+.test {
+background-color: black;
+opacity: 1;
+display:flex;
+   fex-wrap:wrap;
+   flex-direction:row;
+   justify-content:space-around;
+   aling-content:space-around;
+}
+span {
+   display:flex;
+   fex-wrap:wrap;
+   flex-direction:row;
+   justify-content:space-around;
+   aling-content:space-around;
+   color:gold;
+  }
+    p {
+      width:15%;
+      border-radius:10px;
+      box-shadow: 60px 1px blue, -60px 1px red;
+      background-color: grey;
+      border: 2px solid black;
+      color:gold;
+  }
+`
+
+
+const charStats = props => {
+   const {info} = props
+    return (
+        <StyledCharacters>
+         <div className='test'>
+            <span>{info.name}</span>
+              <p>{info.birth_year}</p>
+            </div>
+        </StyledCharacters>
+    )
+}
+
+export default charStats;
